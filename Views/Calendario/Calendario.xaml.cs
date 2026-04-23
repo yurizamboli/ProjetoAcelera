085401385918d451a9c87a94888fd0c82f20c6eb
marjo <marjorye.p.picanco@gmail.com>
2026-04-23 08:07:42 -0300

    Adicionado inicio da tela de calendario

---
+++ Views/Calendario/Calendario.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ProjetoAcelera.Services;
+namespace ProjetoAcelera.Views.Calendario
+{
+    public partial class Calendario : Window 
+    {
+
+    }
+}
